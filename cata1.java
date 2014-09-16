8994aeddf0b82559fe6278f58594bf67b925b1af
JorgeCastellano <jorgecastellanocastellano@gmail.com>
2014-09-16 13:34:47 +0100

    Create cata1.java
    Práctica 1

---
+++ cata1.java
@@ -0,0 +1,11 @@
+package persson;
+
+public class Persson {
+    private String name;
+    private String birthday;
+    private int age;
+        
+    public static void main(String[] args) {
+
+    }
+}
