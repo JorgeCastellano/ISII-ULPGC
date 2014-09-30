ee642875e938dedb11f13af943eae52885e105d7
JorgeCastellano <jorgecastellanocastellano@gmail.com>
2014-09-30 13:32:12 +0100

    Update kata2.java

--- kata2.java
+++ kata2.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class HashMapMostFrequent {
 
 	public static void main(String[] args) {
-		int[] elements = { 1, 3, 4, 1, 5, 2, 3, 6, 6, 6, 4, 1, 2, 6, 2, 3, 1, 2, 1, 5, 5, 1, 1, 5, 4 };
+		int[] elements = { 1,4,7,1,1,8,4,1,4,9 };
 		HashMap<Integer, Integer> frequencyMap = new HashMap<>();
 
 		for (int index = 0; index < elements.length; index++){
@@ -15,16 +15,9 @@ public class HashMapMostFrequent {
 				frequencyMap.put(elements[index], 1);
 			else
 				frequencyMap.put(elements[index], frequency + 1);
+		System.out.println("The most frequent element is " + number + " and it occurs " + times + " times");
+
 		}
 
-		int number = 0;
-		int times = 0;
-		for (Integer key : frequencyMap.keySet())
-			if (frequencyMap.get(key) > times) {
-				number = key;
-				times = frequencyMap.get(key);
-			}
-
-		System.out.println("The most frequent element is " + number + " and it occurs " + times + " times");
 	}
 }
